1a39d04da8671549a7708582667a9d53d7dc1884
Marcel Valeran <marcelvaleran.8@gmail.com>
2020-03-16 14:04:46 +0700

    add image slide promotion

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -8,96 +8,23 @@ import 'view_recommended.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'movie.dart';
 
-class ImageCarousel extends StatefulWidget {
-  _ImageCarouselState createState() => new _ImageCarouselState();
-}
-
-class _ImageCarouselState extends State<ImageCarousel> with SingleTickerProviderStateMixin {
-  Animation<double> animation;
-  AnimationController controller;
-
-  initState() {
-    super.initState();
-    controller = new AnimationController(
-        duration: const Duration(milliseconds: 2000), vsync: this);
-    animation = new Tween(begin: 0.0, end: 18.0).animate(controller)
-      ..addListener(() {
-        setState(() {
-          // the state that has changed here is the animation object’s value
-        });
-      });
-    controller.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    //double screenHeight = MediaQuery.of(context).size.height;
-
-    Widget carousel = new Carousel(
-      boxFit: BoxFit.cover,
-      images: [
-        new AssetImage('lib/images/image_1.png'),
-        new AssetImage('lib/images/image_2.png'),
-        new AssetImage('lib/images/image_3.jpg'),
-        new AssetImage('lib/images/image_4.png'),
-      ],
-      animationCurve: Curves.fastOutSlowIn,
-      animationDuration: Duration(seconds: 1),
-    );
-
-    /*Widget banner = new Padding(
-      padding: const EdgeInsets.only(top: 40.0, left: 20.0),
-      child: new Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(15.0),
-              bottomRight: Radius.circular(15.0)),
-          color: Colors.amber.withOpacity(0.5),
-        ),
-        padding: const EdgeInsets.all(10.0),
-        child: new Text(
-          'AnimeGirl Images',
-          style: TextStyle(
-            fontFamily: 'fira',
-            fontSize: animation.value,//18.0,
-            //color: Colors.white,
-          ),
-        ),
-      ),
-      // ),
-      //  ),
-    );*/
-
-    return new Scaffold(
-      backgroundColor: Colors.blue,
-      body: new Container(
-        padding: const EdgeInsets.only(top: 0),
-        child: new Container(
-          padding: const EdgeInsets.all(0),
-          height: 300,
-          child: new ClipRRect(
-            borderRadius: BorderRadius.circular(3),
-            child: new Stack(
-              children: [
-                carousel,
-                //banner,
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-}
+Widget carousel = new Carousel(
+  boxFit: BoxFit.cover,
+  images: [
+    new AssetImage('lib/images/image_1.png'),
+    new AssetImage('lib/images/image_2.png'),
+    new AssetImage('lib/images/image_3.jpg'),
+    new AssetImage('lib/images/image_4.png'),
+  ],
+  animationCurve: Curves.fastOutSlowIn,
+  animationDuration: Duration(seconds: 1),
+);
 
 class DashboardScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Moviee'),
@@ -111,6 +38,23 @@ class DashboardScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
+            Container(
+              height: 301,
+              padding: const EdgeInsets.only(top: 0),
+              child: new Container(
+                padding: const EdgeInsets.all(0),
+                height: 300,
+                child: new ClipRRect(
+                  borderRadius: BorderRadius.circular(3),
+                  child: new Stack(
+                    children: [
+                      carousel,
+                      //banner,
+                    ],
+                  ),
+                ),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10),
               child: Row(
